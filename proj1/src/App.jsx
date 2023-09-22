13d01cb53c3584eec96cbfef9b910234af47c6d5
prajna_k <pk658@scarletmail.rutgers.edu>
2023-09-22 17:34:43 -0400

    Initial commit

---
+++ proj1/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import Card from './components/Card'
+const App = () => {
+
+  return (
+    
+    <div className="App">
+    <h1>College Essentials</h1>
+  
+        <Card name = "Fairy Lights" image = "./src/assets/fairylights.jpeg" link = "https://www.christmasdesigners.com/led-fairy-lights-125-light-warm-white.html?matchtype=e&network=o&device=c&adposition=&keyword=&campaign=PMax%3A%20Smart%20Shopping%20New&msclkid=20eee13fd4e716e8b2ce8432dfd1e8a2&utm_source=bing&utm_medium=cpc&utm_campaign=PMax%3A%20Smart%20Shopping%20New&utm_term=4575136624019493&utm_content=April%202023"/>
+        <Card name = "Vines" image = "./src/assets/vines.jpeg" link ="https://us.shein.com/1pc-Polyester-Artificial-Leaf-Vine-Minimalist-Artificial-Vine-For-Home-p-14293152-cat-3109.html?url_from=bingpla-us-sh2303280222694765one-size&msclkid=b380d8d2d42a198d0aa2bddf797765d9" ></Card>
+        <Card name = "Shower Caddy" image ="./src/assets/showercaddy.jpeg" link = "https://www.pinterest.com/pin/858498747698979754/"></Card>
+        <Card name = "Poster" image = "./src/assets/poster.jpg" link ="https://www.amazon.co.uk/Jong-Laugh-Flags-Decor-College/dp/B08JHMH624"></Card>
+        <Card name = "Tiny Plants" image = "./src/assets/cactus.jpeg" link = "https://us.shein.com/1pc-Artificial-Potted-Cactus-Small-Plastic-Faux-Bonsai-For-Home-Decor-p-2603031-cat-3098.html?url_from=bingpla-us-shvaf18210428763one-size&msclkid=eecd1fe92ff618d447bc7ae49f0868aa"></Card>
+        <Card name = "Another Cool Poster" image = "./src/assets/lanadelrayposter.jpeg" link = "https://www.amazon.com/dp/B0CGV6QK1T?tag=namespacebran394-20&asc_campaign=2b5a785186b501c352c95398a3440b1b&asc_source=01GW4MT44WE425H3E57X6AJH4A"></Card>
+        <Card name = "Waterbottle" image = "./src/assets/waterbottle.jpeg" link = "https://premierlaserengraving.com/products/32-oz-stainless-steel-polar-camel-water-bottle-personalized?variant=43916899942653"></Card>
+        <Card name = "Backpack" image = "./src/assets/backpack.jpeg" link = "https://www.amazon.com/dp/B07DCXC9S3?tag=namespacebran645-20&asc_source=01H78KDR744R7TP897SJG2W3HM"></Card>
+        <Card name = "Pencils" image = "./src/assets/pencils.webp" link = "https://www.aliexpress.us/item/2251832638168084.html?gatewayAdapt=glo2usa4itemAdapt"></Card>
+        <Card name = "Tissues" image = "./src/assets/tissues.jpeg" link = "https://www.bing.com/images/search?view=detailV2&ccid=gqF47OCB&id=7F761D5DC563E780DBC12EDEA8AD81A4700612E0&thid=OIP.gqF47OCB9TY_0lU2_H1pzgHaHa&mediaurl=https%3a%2f%2fi5.walmartimages.com%2fasr%2f88625ede-9cce-4928-bc80-df108fe277cc_1.f7b1a31da2a6914a003cb8ab524e241d.jpeg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.82a178ece081f5363fd25536fc7d69ce%3frik%3d4BIGcKSBrajeLg%26pid%3dImgRaw%26r%3d0&exph=3000&expw=3000&q=tissues&simid=608004130860062328&FORM=IRPRST&ck=9708BDA9E7C49A81EB157D32B498B4C3&selectedIndex=0&ajaxhist=0&ajaxserp=0"></Card>
+    </div>
+  )
+}
+
+export default App;
